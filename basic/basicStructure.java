0c135b0b50cf1318b4c82e06649a97404ade661b
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-20 16:42:29 +0530

    Learning about java from basic to advance

---
+++ basic/basicStructure.java
@@ -0,0 +1,12 @@
+public class basicStructure {
+    public static void main(String[] args) {
+        System.out.println("Hello wrold\nHow are you?");
+        float x = 5;
+        boolean y = false;
+        if(x == 5){
+            y = true;
+            System.out.println("The Statement is true");
+        }
+        System.out.println("x is equal to "+5+" "+y);
+    }
+}
