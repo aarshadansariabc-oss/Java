615cf319b5dd9334710fb78211395b17927fced4
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-20 16:57:52 +0530

    Learning about array in java

---
+++ Array/initiliaztion.java
@@ -0,0 +1,9 @@
+public class initiliaztion {
+    public static void main(String[] args) {
+        int [] marks = {10,20,30,40,50};
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
